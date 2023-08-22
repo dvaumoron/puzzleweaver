820a901a0143eaff3f87ad7a6475388f85a012f8
dvaumoron <dvaumoron@gmail.com>
2023-08-22 17:18:26 +0200

    add generate script

---
+++ build/generate.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+weaver generate . ./remoteservice ./serviceimpl/admin/ ./serviceimpl/blog/ ./serviceimpl/forum/ ./serviceimpl/login/ ./serviceimpl/markdown/ ./serviceimpl/passwordstrength/ ./serviceimpl/profile/ ./serviceimpl/remotewidget/ ./serviceimpl/salt/ ./serviceimpl/session/ ./serviceimpl/settings/ ./serviceimpl/templates/ ./serviceimpl/wiki/ ./web/common/service/
