ea7a906f15625957c6a7830b2d76487f8bbb8cdd
dvaumoron <dvaumoron@gmail.com>
2023-08-24 18:39:07 +0200

    read default picture

--- frame.go
+++ frame.go
@@ -74,7 +74,7 @@ func frameServe(ctx context.Context, app *frameApp) error {
 	logger := app.Logger(ctx)
 
 	globalConfig := config.New(
-		app.Config(), app, app.sessionService.Get(), app.templateService.Get(), app.settingsService.Get(),
+		app.Config(), app, logger, app.sessionService.Get(), app.templateService.Get(), app.settingsService.Get(),
 		app.passwordStrengthService.Get(), app.saltService.Get(), app.loginService.Get(), app.adminService.Get(),
 		app.profileService.Get(), app.forumService.Get(), app.markdownService.Get(), app.blogService.Get(),
 		app.wikiService.Get(), app.widgetService.Get(),
